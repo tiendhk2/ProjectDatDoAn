d6339654c0035e17127cf45e652a4e0c531bface
tiendhk2 <duongtienpkfr@gmail.com>
2023-07-15 19:16:44 +0700

    jhkjjkl

--- app/src/main/java/tiendhph30203/poly/projectdatdoan/Database/Database.java
+++ app/src/main/java/tiendhph30203/poly/projectdatdoan/Database/Database.java
@@ -30,15 +30,22 @@ public class Database extends SQLiteOpenHelper {
         db.execSQL(dbnguoidung);
 
 
+
+        //Tạo username và password
+        db.execSQL("INSERT INTO nguoidung  VALUES(1,'duongtien','123','Dương Tiến',0332322764,'tiendhph30203@fpt.edu.vn','121 Mỹ Đình','Vip')" +
+                ",(2,'duongyen','123abc123','Dương Yến',0384867860,'duongtienpkfr@gmail.com','123 Mỹ Đình','No Vip')");
+
+
         // Create table Loại Sản Phẩm
         String ctLoaiSanPham = "CREATE TABLE loaisanpham(maloai INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + "tenloai TEXT NOT NULL)";
         db.execSQL(ctLoaiSanPham);
 
-
+        //Tạo loại sản phẩm
+        db.execSQL("INSERT INTO loaisanpham VALUES (1, 'Gà'),(2,'Trà Sữa'),(3, 'Coffee')");
         // Create table Sản Phẩm
         String ctSanPham = "CREATE TABLE sanpham(masanpham INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "anhsanpham BLOG," +
+                "anhsanpham TEXT," +
                 "linkanhsanpham TEXT," +
                 "tensanpham TEXT NOT NULL ," +
                 "giasanpham TEXT NOT NULL ," +
@@ -49,34 +56,30 @@ public class Database extends SQLiteOpenHelper {
                 "hansudung TEXT NOT NULL)";
         db.execSQL(ctSanPham);
 
-
+        //Tạo sản phẩm
+        db.execSQL("INSERT INTO sanpham VALUES (1, 'NONE', 'Link 1', 'KFC', '1000','10',20,1,'01/06/2023','30/06/2023')," +
+                "(2, 'NONE', 'Link 2', 'Tocotoco', '2000','10',20,2,'01/06/2023','30/06/2023'), " +
+                "(3, 'NONE', 'Link 3', 'Bạc xỉu', '3000','10',18,3,'01/06/2023','30/06/2023')");
 
         //Create table Hóa Đơn
         String ctHoaDon = "CREATE TABLE hoadon (" +
                 "mahoadon INTEGER PRIMARY KEY," +
                 "ngaymua TEXT NOT NULL," +
-                "tongtien INTEGER NOT NULL," +
+                "tongtien TEXT NOT NULL," +
                 "trangthai TEXT NOT NULL," +
                 "soluongdamua INTEGER NOT NULL," +
                 "masanpham INTEGER REFERENCES sanpham(masanpham)," +
                 "manguoidung INTEGER REFERENCES nguoidung(manguoidung))";
         db.execSQL(ctHoaDon);
 
-
-        //Tạo username và password
-        db.execSQL("INSERT INTO nguoidung  VALUES(1,'duongtien','123','Dương Tiến',0332322764,'tiendhph30203@fpt.edu.vn','121 Mỹ Đình','Vip')" +
-                ",(2,'duongyen','123abc123','Dương Yến',0384867860,'duongtienpkfr@gmail.com','123 Mỹ Đình','No Vip')");
-
-        //Tạo loại sản phẩm
-        db.execSQL("INSERT INTO loaisanpham VALUES (1, 'Gà'),(2,'Trà Sữa'),(3, 'Coffee')");
-
-        //Tạo sản phẩm
-        db.execSQL("INSERT INTO sanpham VALUES (1, NONE, 'Link 1', 'KFC', 1000,10,20,1,'01/06/2023','30/06/2023')," +
-                "(2, NONE, 'Link 2', 'Tocotoco', 2000,10,20,2,'01/06/2023','30/06/2023'), " +
-                "(3, NONE, 'Link 3', 'Bạc xỉu', 3000,10,18,3,'01/06/2023','30/06/2023')");
-
         //Tạo hóa đơn
-        db.execSQL("INSERT INTO hoadon VALUES(1,'15/07/2023',3000,'Đã giao',1),(2,'20/07/2023',3000,'Chưa giao',2)");
+        db.execSQL("INSERT INTO hoadon VALUES(1,'15/07/2023','3000','Đã giao',1,1,1),(2,'20/07/2023','3000','Chưa giao',2,2,2)");
+
+
+
+
+
+
 
     }
 
